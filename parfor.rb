06fa5866493f2d069fd91d452ef6fb1b9e26b206
Tom Dooner <tomdooner@gmail.com>
2012-04-21 17:03:10 -0400

    Initial

---
+++ parfor.rb
@@ -0,0 +1,16 @@
+require 'json'
+def clean(s)
+    s.downcase.gsub("/","").gsub("(","").gsub(")","").gsub(":","").gsub(".","").gsub(",","").gsub(";","").gsub("?"," ?").gsub("!"," !").gsub("-"," -")
+end
+def search_index(i, query)
+    i.map{|id,review|
+        id if query.split(" ").inject(true){|a,q| a&&review.split(" ").any?{|w| w.include?(q)}}
+    }.compact
+end
+r = {}.tap{|r| File.new(ARGV[0]).readlines.each{|x| y = x.split(" : "); r[y[0]] = clean(y[1])}}
+while true
+    print '>'
+    $stdout.flush
+    query = $stdin.gets
+    print JSON.generate(search_index(r, query))
+end
