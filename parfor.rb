9bacec4552a2466879025176b65a274c0710f877
Tom Dooner <tomdooner@gmail.com>
2012-04-21 18:15:53 -0400

    Minus 8 chars.

--- parfor.rb
+++ parfor.rb
@@ -1,5 +1,5 @@
 require'json'
-def clean(s)s.downcase.gsub(/[\/\(\):\.,;]/,"").gsub("[\-\?!]"," ")end
+def z(s)s.downcase.gsub(/[\/\(\):\.,;]/,"").gsub("[\-\?!]"," ")end
 def s(i,qu)i.map{|j,r|p=qu.split(" ");j if p.inject(true){|a,q|a&&r.any?{|w|w.include?(q)}}}.compact;end
-i={}.tap{|r|File.new($*[0]).readlines.each{|x|y=x.split(" : ");r[y[0]]=clean(y[1..-1].join(" ")).split(" ")}}
+i={}.tap{|r|File.new($*[0]).readlines.each{|x|y=x.split(" : ");r[y[0]]=z(y[1..-1].join(" ")).split(" ")}}
 while true;print'>';STDOUT.flush;q=STDIN.gets;print JSON.generate(s(i,q));end
