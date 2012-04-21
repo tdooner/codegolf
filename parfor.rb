1774ff3dc9b927746fae4ddbb58a7a2ff9355c58
Tom Dooner <tomdooner@gmail.com>
2012-04-21 17:32:46 -0400

    V2

--- parfor.rb
+++ parfor.rb
@@ -1,13 +1,11 @@
 require 'json'
-def clean(s)
-    s.downcase.gsub("/","").gsub("(","").gsub(")","").gsub(":","").gsub(".","").gsub(",","").gsub(";","").gsub("?"," ?").gsub("!"," !").gsub("-"," -")
-end
+def clean(s) s.downcase.gsub("/","").gsub("(","").gsub(")","").gsub(":","").gsub(".","").gsub(",","").gsub(";","").gsub("?"," ?").gsub("!"," !").gsub("-"," -") end
 def search_index(i, query)
     i.map{|id,review|
-        id if query.split(" ").inject(true){|a,q| a&&review.split(" ").any?{|w| w.include?(q)}}
+        id if query.split(" ").inject(true){|a,q| a&&review.any?{|w| w.include?(q)}}
     }.compact
 end
-r = {}.tap{|r| File.new(ARGV[0]).readlines.each{|x| y = x.split(" : "); r[y[0]] = clean(y[1])}}
+r = {}.tap{|r| File.new(ARGV[0]).readlines.each{|x| y = x.split(" : "); r[y[0]] = clean(y[1..-1].join(" ")).split(" ");}}
 while true
     print '>'
     $stdout.flush
