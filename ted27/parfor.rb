0dbbf0601d422a289a5545a508cc31071abb7a13
Tom Dooner <tomdooner@gmail.com>
2012-04-21 19:12:32 -0400

    Even shorter!

--- ted27/parfor.rb
+++ ted27/parfor.rb
@@ -1,3 +1,3 @@
 require'json'
 i={}.tap{|r|File.new($*[0]).readlines.each{|x|y=x.split(" : ");r[y[0]]=y[1..-1].join(" ").downcase.gsub(/[\/\(\):\.,;]/,"").gsub("[\-\?!]"," ")}}
-while true;print'>';STDOUT.flush;q=STDIN.gets;print JSON.generate(i.map{|j,r|p=q.split;j if p.inject(true){|a,s|a&&r.include?(s)}}.compact)end
+while true;print'>';STDOUT.flush;q=STDIN.gets;print JSON.generate(i.map{|j,r|p=q.split;j if p.inject(true){|a,s|a&&!!r[s]}}.compact)end
