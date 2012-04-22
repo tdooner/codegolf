9664c646ae2ffa1ae2f4e98c329986b4117c00de
Tom Dooner <tomdooner@gmail.com>
2012-04-21 20:55:57 -0400

    Shorter yet.

--- ted27/parfor.rb
+++ ted27/parfor.rb
@@ -1,3 +1,3 @@
-require'json'
-i={}.tap{|r|File.new($*[0]).readlines.each{|x|y=x.split(" : ");r[y[0]]=y[1..-1].join(" ").downcase.gsub(/[\/\(\):\.,;]/,"").gsub("[\-\?!]"," ")}}
-while true;print'>';STDOUT.flush;q=STDIN.gets;print JSON.generate(i.map{|j,r|p=q.split;j if p.inject(true){|a,s|a&&r[s]}}.compact)end
+require 'json'
+i={}.tap{|r|open($*[0]).each{|x|y=x.split" :";r[y.shift]=y.join.downcase.gsub(/[\/\(\)\.,;]/,"").gsub("[:\-\?!]"," ")}}
+while 1;print'>';$>.flush;q=STDIN.gets;print JSON.dump(i.map{|j,r|p=q.split;j if p.inject(1){|a,s|a&&r[s]}}.compact)end
